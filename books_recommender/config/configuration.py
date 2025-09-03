1f3315b6b417b731d2945cb7303f13dece8b62a3
tareqrahaman <tareqrahaman565@gmail.com>
2025-09-03 09:11:07 +0600

    data validation pipeline updated

--- books_recommender/config/configuration.py
+++ books_recommender/config/configuration.py
@@ -2,7 +2,7 @@ import os
 import sys
 from books_recommender.logger.log import logging
 from books_recommender.exception.exception_handler import AppException
-from books_recommender.entity.config_entity import DataIngestionConfig
+from books_recommender.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from books_recommender.constant import CONFIG_FILE_PATH
 from books_recommender.utils.util import read_yaml_file
 
@@ -32,6 +32,33 @@ class APPConfiguration:
       logging.info(f"Data Ingestion Config: {response}")
       return response
 
+    except Exception as e:
+      raise AppException(e, sys) from e
+
+  
+  def get_data_validation_config(self) -> DataValidationConfig:
+    try: 
+      artifacts_dir = self.configs_info.artifacts_config.artifacts_dir
+      data_ingestion_config = self.configs_info.data_ingestion_config
+      dataset_dir = data_ingestion_config.dataset_dir
+      ingested_data_dir = data_ingestion_config.ingested_data_dir
+      data_validation_config = self.configs_info.data_validation_config
+      
+      books_csv_file = os.path.join(artifacts_dir,dataset_dir,ingested_data_dir,data_validation_config.books_csv_file)
+      ratings_csv_file = os.path.join(artifacts_dir,dataset_dir,ingested_data_dir,data_validation_config.ratings_csv_file)
+      clean_data_dir = os.path.join(artifacts_dir,dataset_dir,data_validation_config.clean_data_dir)
+      serialized_obj_dir = os.path.join(artifacts_dir,dataset_dir,data_validation_config.serialized_obj_dir)
+
+      response = DataValidationConfig(
+          books_csv_file = books_csv_file,
+          ratings_csv_file = ratings_csv_file,
+          clean_data_dir = clean_data_dir,
+          serialized_obj_dir = serialized_obj_dir
+      )
+
+      logging.info(f"Data Validation Config: {response}")
+      return response
+    
     except Exception as e:
       raise AppException(e, sys) from e
     
--- books_recommender/entity/config_entity.py
+++ books_recommender/entity/config_entity.py
@@ -1,6 +1,11 @@
 from collections import namedtuple
 
-DataIngestionConfig = namedtuple("DatasetConfig", ['dataset_download_url',
+DataIngestionConfig = namedtuple("DataIngestionConfig", ['dataset_download_url',
                                                    'ingested_data_dir',
                                                    'raw_data_dir'])
+
+DataValidationConfig = namedtuple("DataValidationConfig", ["clean_data_dir",
+                                                          "books_csv_file", 
+                                                          "ratings_csv_file",""
+                                                          "serialized_obj_dir"])
 
